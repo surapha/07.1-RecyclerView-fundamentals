d5c7ceb3195a1eed3f86567c897aafd8a3dacff1
USER PC <surapha.nu@rmuti.ac.th>
2020-09-04 22:59:40 +0700

    Step 2: Create the list item layout and text view holder

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -21,8 +21,12 @@ import android.content.res.Resources
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+
+class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
 //import java.util.concurrent.TimeUnit
 //import java.util.*
 
